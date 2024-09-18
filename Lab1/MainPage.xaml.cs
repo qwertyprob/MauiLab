b826ab13487e3ef42dac7a47696653817b484b4a
qwertyprob <eqspertars@gmail.com>
2024-09-19 00:41:52 +0300

    Finished 2 methods and one eventsender

--- Lab1/MainPage.xaml.cs
+++ Lab1/MainPage.xaml.cs
@@ -1,14 +1,45 @@
-﻿namespace Lab1
+﻿using Microsoft.Maui;
+
+namespace Lab1
 {
     public partial class MainPage : ContentPage
     {
-
         public MainPage()
         {
             InitializeComponent();
         }
 
         
+        public double CalculateBMI(double weight, double height)
+        {
+            if (height <= 0)
+                throw new ArgumentException("Height must be greater than zero.");
+
+            return weight / (height * height);
+        }
+
+       
+        void OnCalculateClicked(object sender, EventArgs e)
+        {
+            try
+            {
+                
+                double weight = double.Parse(weightEntry.Text);
+                double height = double.Parse(heightEntry.Text);
+
+                
+                double bmi = CalculateBMI(weight, height);
+
+                
+                bmiLabel.Text = $"ИМТ: {bmi:F2}";
+            }
+            catch (Exception ex)
+            {
+                
+                bmiLabel.Text = "Ошибка в расчёте ИМТ: Введите правильные числа";
+            }
+        }
     }
+
 
 }
--- Lab1/obj/Debug/net8.0-maccatalyst/maccatalyst-x64/Lab1.AssemblyInfo.cs
+++ Lab1/obj/Debug/net8.0-maccatalyst/maccatalyst-x64/Lab1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+425eeda37136c563c4087838901a7c0b64fd4b42")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
